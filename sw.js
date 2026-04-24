cf2bb0858667eed16866e1b030bf1c15611ef8b9
vitaltrading09 <vitaltrading09@gmail.com>
2026-04-24 21:07:08 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE = 'koekela-v3';
+const PRECACHE = [
+  './Koekela%20App.html',
+  './Koekela%20Dashboard.html',
+  './manifest.json',
+  './icon.svg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(PRECACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  const isLocal = url.origin === self.location.origin;
+
+  if (isLocal) {
+    // Cache-first for own files (app works offline immediately)
+    e.respondWith(
+      caches.match(e.request).then(cached => {
+        const network = fetch(e.request).then(res => {
+          if (res.ok) {
+            const clone = res.clone();
+            caches.open(CACHE).then(c => c.put(e.request, clone));
+          }
+          return res;
+        }).catch(() => cached);
+        return cached || network;
+      })
+    );
+  } else {
+    // Network-first for CDN (fonts, React, Babel) with cache fallback
+    e.respondWith(
+      fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+  }
+});
